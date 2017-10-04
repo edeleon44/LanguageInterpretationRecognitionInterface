78b6d8695f915e0505825d88a1ebfa98a1cf5528
Edward DeLeon Jr <edwarddeleon@Edwards-MacBook-Pro.local>
2017-10-04 18:04:49 -0500

    Push Number One

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+
+var twitterKeys = {
+  consumer_key: '56piPVJYKlmsVHSIgzJo7xkRr',
+  consumer_secret: 'IK4h4KT2RWg8jo5uaCk5teEdReV55fGc5LVdi95uLZZYRm1MZf',
+  access_token_key: '913939701136011264-z2JSGzOGARBeDJyDvVW6yZR9wkmpQeM',
+  access_token_secret: 'clbM15EssvmhyWPeCBO27obcW59zKiAk5xIum9CCv5x7L',
+}
+//console.log('this is loaded');
+module.exports = twitterKeys;
+
---
+++ liri.js
@@ -0,0 +1,73 @@
+var keys = require('./keys.js');
+var fs = require('fs');
+var twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: 'b37ab4679b86470bb962af9a9396d1c7',
+  secret: '2ab34597f7e4477faa7a5e671015c67b',
+});
+// var request = require('request');
+//
+function myTweets() {
+	var twitterUsername = process.argv[3];
+	var params = {screen_name: "DayToDayPoker"}; 
+	var client = new twitter(keys);
+	//console.log(keys)
+	client.get("statuses/user_timeline/", params, function(error,data,response){
+		if(!error){
+      for(var i = 0; i < data.length; i++){
+        var date = data[i].created_at;
+        console.log("-----------------------")
+        console.log("@DayToDayPoker: " + data[i].text) 
+        console.log(" Created At: " + date.substring(0, 19));
+        console.log("-----------------------")
+    }
+}
+
+		//console.log(data)
+
+	});
+}
+
+myTweets();	
+
+//
+
+
+//
+
+function getMovie(){
+var userMovie = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&tomatoes=true&r=json&apikey=40e9cece";
+var movie = 'Mr.Nobody';
+var request = require('request');
+    request(userMovie, function(error, response, body) {
+          if (!error && response.statusCode === 200) 
+     var movieObject = JSON.parse(body);
+    console.log("Title: " + movieObject.Title); 
+    console.log("Year: " + movieObject.Year);
+    console.log("IMDB Rating: " + movieObject.imdbRating);
+    console.log("Country: " + movieObject.Country);
+    console.log("Language: " + movieObject.Language);
+    console.log("Plot: " + movieObject.Plot);
+    console.log("Actors: " + movieObject.Actors);
+    console.log("Tomato Rating: " + movieObject.tomatoRating);
+    console.log("Tomato URL: " + movieObject.tomatoURL);
+
+      //console.log(movieObject);
+
+
+})
+};
+getMovie();
+
+//
+
+spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+
+});
